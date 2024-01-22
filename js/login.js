4f739d1a4220a87c525ad254dfc11fe6f19380bb
saminur633 <rahmansaminur51@gmail.com>
2024-01-23 01:33:07 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,8 @@
+// step:1
+document.getElementById('btn-submit').addEventListener('click', function(){
+    const emailField = document.getElementById('user-email');
+    const email = emailField.value;
+    const passwordField = document.getElementById('user-password');
+    const password = passwordField.value;
+    console.log(email,password);
+})
